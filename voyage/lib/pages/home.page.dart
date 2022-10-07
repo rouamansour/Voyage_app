73a1f33e623386efc2a50387051952c88e474014
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-07 11:17:36 -0700

    add button logout in file home

--- voyage/lib/pages/home.page.dart
+++ voyage/lib/pages/home.page.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget{
-  const HomePage({Key? key}): super(key:key);
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatelessWidget {
+  //const HomePage({Key? key}): super(key:key);
+  late SharedPreferences prefs;
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Page Home')),
-      body: Center(
-        child:Text(
-          'Home Page ',
-          style:TextStyle(fontSize: 22),
-        )) );
-    
+        appBar: AppBar(title: Text('Page Home')),
+        body: Center(
+            child: Container(
+          padding: EdgeInsets.all(10),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50)),
+              onPressed: () {
+                _Deconnexion(context);
+              },
+              child: Text('Deconnexion', style: TextStyle(fontSize: 22))),
+        )));
+  }
+
+  Future<void> _Deconnexion(context) async {
+    prefs = await SharedPreferences.getInstance();
+    prefs.setBool("connect", false);
+    Navigator.pop(context);
+    Navigator.pushNamed(context, '/authentification');
   }
 }
