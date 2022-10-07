2af17d0ee888ae0fe7ab2ddc3605f5c7db19ae61
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-07 09:47:36 -0700

    add form for file authentification

--- voyage/lib/pages/authentification.page.dart
+++ voyage/lib/pages/authentification.page.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
 class AuthentificationPage extends StatelessWidget {
-  const AuthentificationPage({Key? key}) : super(key: key);
+  // constructor
+  // const AuthentificationPage({Key? key}) : super(key: key);
+  TextEditingController txt_login=new TextEditingController();
+  TextEditingController txt_password=new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Page Authentification')),
-        body: Center(
-            child: Text(
-          'Authentification Page ',
-          style: TextStyle(fontSize: 22),
-        )));
+        body: Column(
+        children:[
+          Container(
+            padding: EdgeInsets.all(10),
+            child:TextFormField(
+              controller: txt_login,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                hintText: "utilisateur",
+                border:OutlineInputBorder(
+                  borderSide: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child:TextFormField(
+                obscureText: true,
+                controller: txt_password,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.password),
+                  hintText: "Mot de passe",
+                  border: OutlineInputBorder(
+                    borderSide:BorderSide(width:1),
+                    borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+              Container(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50)),
+                  onPressed: () {},
+                  child: Text('Connexion',style: TextStyle(fontSize: 22))),
+                ),
+               TextButton(
+                  child: Text("Nouvel utilisateur",
+                  style: TextStyle(fontSize: 22)),
+                  onPressed: (() {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/inscription');
+                  })) 
+        ],
+        ));
   }
 }
