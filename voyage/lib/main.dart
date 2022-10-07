a1bfb878e17fe8b686e2b5abf560dd57f8bbc232
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-07 11:26:52 -0700

    update file main

--- voyage/lib/main.dart
+++ voyage/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:voyage/pages/authentification.page.dart';
 import 'package:voyage/pages/home.page.dart';
 import 'package:voyage/pages/inscription.page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +14,25 @@ class MyApp extends StatelessWidget {
   };
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes:routes,
-      home: InscriptionPage(),
-    );
+    return FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: 
+        (BuildContext context, AsyncSnapshot<SharedPreferences> prefs){
+          var x=prefs.data;
+          if(prefs.hasData){
+            bool conn=x?.getBool('connecte')??false;
+            if(conn)
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home:HomePage(),
+              routes:routes,
+            );
+          }
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: AuthentificationPage(),
+            routes: routes,
+          );
+        });
   }
 }
