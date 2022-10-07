7661b789fad61165f8d27ee5cfd079478c8b4b8c
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-07 09:43:40 -0700

    add routes for file main

--- voyage/lib/main.dart
+++ voyage/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:voyage/pages/authentification.page.dart';
+import 'package:voyage/pages/home.page.dart';
 import 'package:voyage/pages/inscription.page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final routes={
+    '/home':(context)=> HomePage(),
+    '/inscription':(context) =>InscriptionPage(),
+    '/authentification':(context)=> AuthentificationPage(),
+  };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes:routes,
       home: InscriptionPage(),
     );
   }
