91654885a4ae9c05c8714cc59d092bf8789f8f76
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-13 09:31:33 +0000

    add list menu

--- voyage/lib/pages/home.page.dart
+++ voyage/lib/pages/home.page.dart
@@ -29,6 +29,8 @@ class HomePage extends StatelessWidget {
     Navigator.pushNamed(context, '/authentification');
   }
 }
+
+
 class MyDrawer extends StatelessWidget{
   const MyDrawer({Key? key}): super(key:key);
   @override
@@ -43,7 +45,49 @@ class MyDrawer extends StatelessWidget{
                     child: CircleAvatar(
                       backgroundImage: AssetImage("images/profile.jpg"),
                     ),
-                  ))
+                  )),
+          ListTile(
+            title: Text('Acceuil',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.home,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Meteo',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.sunny_snowing,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Gallerie',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.photo,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Pays',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.location_city,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Contact',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.contact_page,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Parametre',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.settings,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
+           ListTile(
+            title: Text('Deconnexion',style: TextStyle(fontSize: 22)),
+            leading: Icon(Icons.logout,color:Colors.blue),
+            trailing: Icon(Icons.arrow_right,color:Colors.blue),
+            onTap: (){},
+          ),
         ],
       ));
   }
