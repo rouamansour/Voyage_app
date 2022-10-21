78cb7d2fe192ed351f426b334a54b7a1ce2eaac6
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-21 08:41:20 +0000

    update file pubspec and file main

--- voyage/lib/main.dart
+++ voyage/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:voyage/pages/authentification.page.dart';
+import 'package:voyage/pages/contact.page.dart';
+import 'package:voyage/pages/gallerie.page.dart';
 import 'package:voyage/pages/home.page.dart';
 import 'package:voyage/pages/inscription.page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:voyage/pages/meteo.page.dart';
+import 'package:voyage/pages/parametres.page.dart';
+import 'package:voyage/pages/pays.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +18,11 @@ class MyApp extends StatelessWidget {
     '/home':(context)=> HomePage(),
     '/inscription':(context) =>InscriptionPage(),
     '/authentification':(context)=> AuthentificationPage(),
+    '/meteo':(context)=>MeteoPage(),
+    '/gallerie':(context)=>GalleriePage(),
+    '/pays':(context)=>PaysPage(),
+    '/contact':(context)=> ContactPage(),
+    '/parametres':(context)=>ParametrePage(),
   };
   @override
   Widget build(BuildContext context) {
