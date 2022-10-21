c818be11cbd5a9474081eb81e6beafde5262f225
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-21 08:57:23 +0000

    update meteo

--- voyage/lib/pages/meteo.page.dart
+++ voyage/lib/pages/meteo.page.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
-
 import '../menu/drawer.widget.dart';
 
 class MeteoPage extends StatelessWidget{
+  TextEditingController txt_ville = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer:MyDrawer(),
-        appBar: AppBar(title: Text(' Meteo')),
-        body: Center(
-            child: Text(
-              'Page meteo',
-              style: Theme.of(context).textTheme.headline3,
-        ),
-        ),
-        );
+      appBar: AppBar(
+        title: Text('Météo'),
+         ),
+         body: Column (
+          children : [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                controller: txt_ville,
+                decoration: InputDecoration(prefixIcon: Icon (Icons.location_city),
+                hintText: "Ville",
+                border : OutlineInputBorder (
+                  borderSide: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(10))),
+            ),
+          ),
+          Container(padding: EdgeInsets.all(10),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50)),
+              onPressed: (){},
+              child: Text('Chercher', style: TextStyle(fontSize: 22))),
+              ),
+          ],
+    ));
   }
+
 }
