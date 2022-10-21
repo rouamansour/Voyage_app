d06f3855fcbdbd4b01b9842e3c04eff34a8c1a26
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-21 08:43:17 +0000

    create file drawer

--- voyage/lib/menu/drawer.widget.dart
+++ voyage/lib/menu/drawer.widget.dart
@@ -1,64 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:voyage/config/global.params.dart';
 
-class MyDrawer extends StatelessWidget{
-  const MyDrawer({Key? key}): super(key:key);
+class MyDrawer extends StatelessWidget {
+  late SharedPreferences prefs;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [Colors.white,Colors.blue])),
-                  child: Center(
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage("images/profile.jpg"),
-                    ),
-                  )),
-          ListTile(
-            title: Text('Acceuil',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.home,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Meteo',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.sunny_snowing,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Gallerie',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.photo,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Pays',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.location_city,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Contact',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.contact_page,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Parametre',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.settings,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-           ListTile(
-            title: Text('Deconnexion',style: TextStyle(fontSize: 22)),
-            leading: Icon(Icons.logout,color:Colors.blue),
-            trailing: Icon(Icons.arrow_right,color:Colors.blue),
-            onTap: (){},
-          ),
-        ],
-      ));
+        child: ListView(
+          children: [
+            DrawerHeader(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.white, Colors.blue])),
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("images/profile.jpg"),
+                    radius: 80,
+                  ),
+                )),
+            ...(GlobalParams.menus as List).map((item) {
+              return ListTile(
+                title: Text(
+                  '${item['title']}', style: TextStyle(fontSize: 22),),
+                leading: item['icon'],
+                trailing: Icon(Icons.arrow_right, color: Colors.blue,),
+                onTap: () async {
+                  if ('${item['title']}' != "Déconnexion") {
+                    Navigator.of(context).pop();
+                    Navigator.pushNamed(context, "${item ['route']}");
+                  }
+                  else {
+                    prefs = await SharedPreferences.getInstance();
+                    prefs.setBool("connecte", false);
+                    Navigator.of(context).pushNamedAndRemoveUntil('/authentification', (Route<dynamic> route) => false);
+                  }
+                },
+              );
+            })
+          ],
+        ));
   }
 }
