2405285ec677cbd67d7d8e09cd5c2f09b62ce246
Roua Mansour <rouamansour.rm@gmail.com>
2022-10-21 08:44:07 +0000

    update home

--- voyage/lib/pages/home.page.dart
+++ voyage/lib/pages/home.page.dart
@@ -12,16 +12,71 @@ class HomePage extends StatelessWidget {
         drawer:MyDrawer(),
         appBar: AppBar(title: Text('Page Home')),
         body: Center(
-            child: Container(
-          padding: EdgeInsets.all(10),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50)),
-              onPressed: () {
-                _Deconnexion(context);
-              },
-              child: Text('Deconnexion', style: TextStyle(fontSize: 22))),
-        )));
+            child: Wrap(
+              children:[
+                InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/meteo.png',),),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/meteo');
+                      },
+                    ),
+                    InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/gallerie.png',),),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/gallerie');
+                      },
+                    ),
+                    InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/pays.png',),),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/pays');
+                      },
+                    ),
+                    InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/contact.png',),),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/contact');
+                      },
+                    ),
+                    InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/parametres.png',),),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/parametres');
+                      },
+                    ),
+                    InkWell(
+                    child: Ink.image(
+                      height: 180,
+                      width: 180,
+                      image: AssetImage('images/deconnexion.png',),),
+                      onTap: () {
+                        _Deconnexion(context);
+                      },
+                    ),
+              ],
+            ),
+        )
+    );
   }
 
   Future<void> _Deconnexion(context) async {
