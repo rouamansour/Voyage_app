55c8f555e9943ac99ab79457121bcfe78ed48ee9
Roua Mansour <rouamansour.rm@gmail.com>
2022-11-24 23:44:13 +0000

    update page pays

--- voyage/lib/pages/pays.page.dart
+++ voyage/lib/pages/pays.page.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:voyage/pages/pays-details.page.dart';
 
 import '../menu/drawer.widget.dart';
 
-class PaysPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
+class PaysPage extends StatelessWidget {
+  TextEditingController txt_pays = new TextEditingController();
+  Widget build(BuildContext context){
     return Scaffold(
-        drawer:MyDrawer(),
-        appBar: AppBar(title: Text(' Pays')),
-        body: Center(
-            child: Text(
-              'Page Pays',
-              style: Theme.of(context).textTheme.headline3,
-        ),
-        ),
-        );
+        appBar: AppBar(title: Text('Page Pays')),
+        body: Column(
+          children: [
+
+            Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: txt_pays,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.location_pin),
+                      hintText: "pays",
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(10)
+                      )
+                  ),
+                )
+            ),
+
+            Container(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50)),
+                  onPressed: () {
+                    _onGetPaysDetails(context);
+                  },
+                  child: Text('Chercher', style: TextStyle(fontSize: 22))),
+            ),
+
+          ],
+        )
+    );
+  }
+
+  void _onGetPaysDetails(BuildContext context) {
+    String p=txt_pays.text;
+    Navigator.push(context,
+        MaterialPageRoute(
+          builder: (context) => PaysDetailsPage(p)));
+    txt_pays.text="";
   }
 }
